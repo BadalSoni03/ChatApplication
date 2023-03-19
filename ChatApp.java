6c791ba07e067e82809812002907647cb76d3b16
BadalSoni03 <111721917+BadalSoni03@users.noreply.github.com>
2023-03-19 19:41:50 +0530

    Add files via upload

---
+++ ChatApp.java
@@ -0,0 +1,10 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfig.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApp.class , args);
+    }
+}
